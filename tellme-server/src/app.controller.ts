f7d161487d2a9aec341d2f5794053d200a9918e0
avtushko2004 <avtushko2004@gmail.com>
2023-12-13 01:19:48 +0300

    add redis

--- tellme-server/src/app.controller.ts
+++ tellme-server/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(CACHE_MANAGER) private cacheManager,
+  ) {}
 
   @Get()
-  getHello(): string {
+  async set() {
     return this.appService.getHello();
   }
 }
--- tellme-server/src/app.module.ts
+++ tellme-server/src/app.module.ts
@@ -1,9 +1,23 @@
+import { redisStore } from 'cache-manager-redis-store';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line prettier/prettier
+      store: async () => await redisStore({
+          socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+        }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
